0adc5de89e15cbc9356b1a1c50df0e705e6f553d
diaseric <42677871+diaseric@users.noreply.github.com>
2019-04-01 12:56:13 -0700

    Create Escort.java

---
+++ Escort.java
@@ -0,0 +1,19 @@
+public class Escort extends Starship implements Starship.Stance {
+    public Escort(String newName) {
+        int minHull = 1000;
+        int maxHull = 1250;
+        int minShield = 2500;
+        int maxShield = 3500;
+        int minBeam = 2500;
+        int maxBeam = 3000;
+        int minTorp = 10;
+        int maxTorp = 20;
+
+
+
+
+    }
+    public String Stance(){
+        return "Defensive";
+    }
+}
