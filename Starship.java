c94a5d33316175833cd570f890ebffc39a97fe52
diaseric <42677871+diaseric@users.noreply.github.com>
2019-04-01 12:55:50 -0700

    Create Starship.java

---
+++ Starship.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.Random;
+
+public class Starship {
+    public String escortShipName(String EshipName){
+        Random rand = new Random();
+        int hull = rand.nextInt(minHull, maxHull);
+
+    }
+
+    public interface Stance{
+        String Stance();
+    }
+
+    List<String> ships = new ArrayList<>();
+
+
+
+
+
+
+}
